ec10ccd14601a72801d783435668d44a84c3aab4
devz906 <106856910+devz906@users.noreply.github.com>
2026-02-22 02:40:46 +0000

    A18 Pro: Final transformation to Executable Target

--- Shell/Package.swift
+++ Shell/Package.swift
@@ -5,13 +5,12 @@ let package = Package(
     name: "Shell",
     platforms: [.iOS("17.0")],
     products: [
-        .library(name: "WineKit", targets: ["WineKit"])
+        .executable(name: "WineKit", targets: ["WineKit"])
     ],
     targets: [
-        .target(
+        .executableTarget(
             name: "WineKit",
             path: "Sources/WineKit",
-            sources: ["UI", "Logger.swift"],
             resources: [.process("Resources")]
         )
     ]
---
+++ Shell/Sources/WineKit/main.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+struct WineKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
+WineKitApp.main()
